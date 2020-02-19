6a106db784d1f410cb8678e7773b8eb38bc915b1
Elian Van Cutsem <elian.vancutsem@student.odisee.be>
2020-02-19 14:11:08 +0100

    automized login

--- pivo.py
+++ pivo.py
@@ -1,22 +1,33 @@
 # script written by Elian Van Cutsem
 
 #imports
-import webbrowser
-#from selenium import webdriver
-
-#start of program
-#webbrowser.open('http://www.google.com')
+import time
+# you need to install selenium for this
+from selenium import webdriver
 
 #variables
 login = ""
 password = ""
 
-def getCredentials():
+#functions
+def getCredentials(i):
     readOut = []
     with open ('credentials.txt') as textFile:
         for line in textFile :
             readOut += line.rstrip("\n").split(" ")
-        login = readOut[0]
-        password = readOut[1]
+        return readOut[i]
 
-getCredentials()
+def login():
+    login = getCredentials(0)
+    password = getCredentials(1)
+    loginElement = browser.find_element_by_id('username')
+    loginElement.send_keys(login)
+    passElement = browser.find_element_by_id('password')
+    passElement.send_keys(password)
+    passElement.submit()
+
+#running of program
+# you need to install geckodriver for this
+browser = webdriver.Firefox()
+browser.get('https://www.vlaamsbrabant.be/promarUD/login.do')
+login()

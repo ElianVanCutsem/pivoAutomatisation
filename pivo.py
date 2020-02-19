6de4d52ad5a9a3d0e938b94695a852e3d44d542c
Elian Van Cutsem <elian.vancutsem@student.odisee.be>
2020-02-19 14:56:06 +0100

    added additem function and checkup function

--- pivo.py
+++ pivo.py
@@ -1,13 +1,19 @@
+# this script can be used to make a reservation on the pivo website
 # script written by Elian Van Cutsem
 
 #imports
 import time
-# you need to install selenium for this
+
+# you need to install selenium for following imports
+# pip install selenium
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 #variables
-login = ""
-password = ""
+soiree = [
+"XLR kabelset 10stuks van 10m in koffer",
+"standaard microstatief"
+]
 
 #functions
 def getCredentials(i):
@@ -26,14 +32,27 @@ def login():
     passElement.send_keys(password)
     passElement.submit()
 
+def clearInput(webElement):
+    webElement.send_keys(Keys.CONTROL + "a");
+    webElement.send_keys(Keys.DELETE);
+
 def addNewProduct(productToAdd):
     zoekTerm = browser.find_element_by_id('keyword')
     zoekTerm.send_keys(productToAdd)
     zoekTerm.submit()
     time.sleep(1)
-    print(browser.find_elements_by_xpath('/html/body/div[2]/div[4]/div[1]/div[2]/div[1]/a[2]'))
     addButton = browser.find_elements_by_xpath('/html/body/div[2]/div[4]/div[1]/div[2]/div[1]/a[2]')[0]
     addButton.click()
+    zoekTerm = browser.find_element_by_id('keyword')
+    clearInput(zoekTerm)
+
+def addItems(list):
+    for x in range(0, len(list)):
+        addNewProduct(list[x])
+
+def doCheckup():
+    reservationButton = browser.find_elements_by_xpath('/html/body/div[2]/div[3]/a')[0]
+    reservationButton.click()
 
 #running of program
 # you need to install geckodriver for this
@@ -41,4 +60,5 @@ browser = webdriver.Firefox()
 browser.get('https://www.vlaamsbrabant.be/promarUD/login.do')
 login()
 time.sleep(3)
-addNewProduct("XLR kabelset 10stuks van 10m in koffer")
+addItems(soiree)
+doCheckup()
